a21943b04adca652e7597a506f20c7093acaf533
JNamasaki(Jessica Rodrigues) <r.jmrsilva.j@gmail.com>
2024-02-25 12:22:56 -0300

    Estudando quick sort

---
+++ Sort/quickSort.java
@@ -0,0 +1,34 @@
+package Sort;
+
+public class quickSort {
+    public static int[] quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int pivotIndex = partition(arr, left, right);
+            quickSort(arr, left, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, right);
+        }
+        return arr;
+    }
+
+    private static int partition(int[] arr, int left, int right) {
+        int pivot = arr[right];
+        int i = left - 1;
+
+        for (int j = left; j < right; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i + 1, right);
+        return i + 1;
+    }
+
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
---
+++ main/MainClass.java
@@ -0,0 +1,26 @@
+package main;
+
+import Sort.quickSort;
+
+public class MainClass {
+    public static void main(String[] args) {
+        int[] arr = {10, 7, 8, 9, 1, 5,13,28,6};
+        int n = arr.length;
+
+
+        System.out.println("Array original:");
+        printArray(arr);
+
+         quickSort.quickSort(arr, 0, n - 1);
+
+        System.out.println("Array ordenado:");
+        printArray(arr);
+    }
+
+    private static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
